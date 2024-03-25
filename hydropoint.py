b129ad0406f7181d31a99872c0030171db5531c0
Robert Edwin Rouse <robert.edwin.rouse@gmail.com>
2024-03-25 09:57:15 +0000

    update

--- hydropoint.py
+++ hydropoint.py
@@ -57,15 +57,14 @@ class hydrobase():
                                                         self.gridsquare[2],
                                                         self.gridsquare[3],)
         
-    def meteorological_extraction(self, domain_data, method):
-        local_data = domain_data.interp(longitude=self.lon, latitude=self.lat,
-                                        method=method, order=method)
+    def meteorological_extraction(self, domain_data):
+        local_data = domain_data.interp(longitude=self.lon, latitude=self.lat)
         local_data = local_data.to_dataframe().reset_index()
         return local_data
     
     def flow_meteorolgy_combine(self, domain_weather, surface_data, days):
-        weather = self.meteorological_extraction(domain_weather, 'cubic')
-        surface = self.meteorological_extraction(surface_data, 'cubic')
+        weather = self.meteorological_extraction(domain_weather)
+        surface = self.meteorological_extraction(surface_data)
         weather = rename(weather)
         surface = rename(surface)
         weather = pd.to_datetime(weather['Date'], format='%Y-%m-%d').dt.date
