f1c563884190e62d3fb157ffd7c50a3bc87e65ba
Robert Edwin Rouse <robert.edwin.rouse@gmail.com>
2024-03-25 16:59:14 +0000

    Updated hydropoint

--- hydropoint.py
+++ hydropoint.py
@@ -19,6 +19,19 @@ from apollo import centroid as cr
 
 
 def rename(df):
+    '''
+    
+
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    df : TYPE
+        DESCRIPTION.
+    '''
     df = df.rename(columns={'time':'Date','tp':'Rain','t':'Temperature',
                         'u':'U Windspeed','v':'V Windspeed','r':'Humidity',
                         'swvl1':'Soil Moisture 1','swvl2':'Soil Moisture 2',
@@ -27,6 +40,25 @@ def rename(df):
 
 
 def weather_shift(xf, variable, ndays, past=True):
+    '''
+    
+
+    Parameters
+    ----------
+    xf : TYPE
+        DESCRIPTION.
+    variable : TYPE
+        DESCRIPTION.
+    ndays : TYPE
+        DESCRIPTION.
+    past : TYPE, optional
+        DESCRIPTION. The default is True.
+
+    Returns
+    -------
+    xf : TYPE
+        DESCRIPTION.
+    '''
     if past == True:
         for i in range (1, ndays):
             colname = str(variable) + '-{0}'.format(i)
@@ -40,6 +72,24 @@ def weather_shift(xf, variable, ndays, past=True):
 
 class hydrobase():
     def __init__(self, station, flowpath, gridfile, gridsquare):
+        '''
+        
+
+        Parameters
+        ----------
+        station : TYPE
+            DESCRIPTION.
+        flowpath : TYPE
+            DESCRIPTION.
+        gridfile : TYPE
+            DESCRIPTION.
+        gridsquare : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+        '''
         self.station = station
         self.flowpath = flowpath
         self.flow = pd.read_csv(flowpath)
@@ -58,16 +108,46 @@ class hydrobase():
                                                         self.gridsquare[3],)
         
     def meteorological_extraction(self, domain_data):
+        '''
+        
+
+        Parameters
+        ----------
+        domain_data : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        local_data : TYPE
+            DESCRIPTION.
+        '''
         local_data = domain_data.interp(longitude=self.lon, latitude=self.lat)
         local_data = local_data.to_dataframe().reset_index()
         return local_data
     
     def flow_meteorolgy_combine(self, domain_weather, surface_data, days):
+        '''
+        
+
+        Parameters
+        ----------
+        domain_weather : TYPE
+            DESCRIPTION.
+        surface_data : TYPE
+            DESCRIPTION.
+        days : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        combined : TYPE
+            DESCRIPTION.
+        '''
         weather = self.meteorological_extraction(domain_weather)
         surface = self.meteorological_extraction(surface_data)
         weather = rename(weather)
         surface = rename(surface)
-        weather['Rain'] = weather['Rain']*1000
+        weather['Rain'] = weather['Rain']*1000*24
         weather['Date'] = pd.to_datetime(weather['Date'],
                                          format='%Y-%m-%d').dt.date
         surface['Date'] = pd.to_datetime(surface['Date'],
@@ -82,9 +162,26 @@ class hydrobase():
                 me.stat_roller(weather, f, window, method='mean')
         combined = pd.merge(weather, surface, on='Date')
         combined = pd.merge(self.flow, combined, how='inner', on='Date')
+        combined = combined.drop(combined.index[0:178])
         return combined
     
     def output_file(self, domain_weather, surface_data, days):
+        '''
+        
+
+        Parameters
+        ----------
+        domain_weather : TYPE
+            DESCRIPTION.
+        surface_data : TYPE
+            DESCRIPTION.
+        days : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+        '''
         outdf = self.flow_meteorolgy_combine(domain_weather, surface_data, days)
         outpath = self.station + '_lumped.csv'
         outdf.to_csv(outpath, index=True)
